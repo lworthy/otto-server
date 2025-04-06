234497833ddb342d577aac5ac83169937e26132a
Landen Worthy <lworthy@email.sc.edu>
2025-04-06 13:42:05 -0400

    added cors

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const menuItems = require('./data');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Serve static files (like index.html)
+// 🔥 Add this line to allow cross-origin requests
+app.use(cors());
+
+// Serve static files like index.html
 app.use(express.static(path.join(__dirname, 'public')));
 
 // API route
@@ -13,7 +17,6 @@ app.get('/api/menu', (req, res) => {
   res.json(menuItems);
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
